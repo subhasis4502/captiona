766b899911949cda6935d3cc72176f36150995a6
subhasis4502 <subhasis4502@gmail.com>
2021-10-20 13:24:12 +0530

    final

--- client/src/config.js
+++ client/src/config.js
@@ -1,5 +1,5 @@
 import axios from "axios"
 
 export const axiosInstance = axios.create({
-    baseURL: "https://lamasocial.herokuapp.com/api/"
+    baseURL: "https://captiona.herokuapp.com/api/"
 })
